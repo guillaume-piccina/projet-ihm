da6adbdc2c38e1ea531611209c5bb25b01d2e535
NaudEric <eric.naud@etu.unice.fr>
2021-04-04 22:43:24 +0200

    buttons

--- app/src/main/java/projet/ihm/MainActivity.java
+++ app/src/main/java/projet/ihm/MainActivity.java
@@ -2,6 +2,7 @@ package projet.ihm;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
