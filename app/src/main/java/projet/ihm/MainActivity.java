4c1a13692fdc4197dc211d657c3a9555a9f0318e
NaudEric <eric.naud@etu.unice.fr>
2021-04-04 22:56:06 +0200

    activités

--- app/src/main/java/projet/ihm/MainActivity.java
+++ app/src/main/java/projet/ihm/MainActivity.java
@@ -3,6 +3,7 @@ package projet.ihm;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,5 +12,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        (findViewById(R.id.buttonProfile)).setOnClickListener( click -> {
+            Intent intentSend = new Intent( getApplicationContext(), ProfileActivity.class);
+            startActivity(intentSend);
+        });
+
+        (findViewById(R.id.buttonReport)).setOnClickListener( click -> {
+            Intent intentSend = new Intent( getApplicationContext(), ReportActivity.class);
+            startActivity(intentSend);
+        });
+
+        (findViewById(R.id.buttonCall)).setOnClickListener( click -> {
+            Intent intentSend = new Intent( getApplicationContext(), CallActivity.class);
+            startActivity(intentSend);
+        });
+
     }
 }
