210b0a136aae473991ceae67c0a0d07b4d438afe
NaudEric <eric.naud@etu.unice.fr>
2021-04-27 15:24:47 +0200

    #annuler dernier commit

--- app/src/main/java/projet/ihm/controller/IGPSActivity.java
+++
@@ -1,6 +0,0 @@
-package projet.ihm.controller;
-
-public interface IGPSActivity {
-    int REQUEST_CODE = 400;  // request code used in requestPermissions() méthode and its callback onRequestPermissionsResult()
-    void moveCamera();      // move camera (with zoom) to center the map to the GPS position
-}
