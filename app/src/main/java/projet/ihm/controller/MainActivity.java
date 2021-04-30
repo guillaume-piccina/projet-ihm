cf6990761a5706bf19e065d621f9001ba58e8b08
NaudEric <eric.naud@etu.unice.fr>
2021-04-30 18:58:39 +0200

    #appel 18

--- app/src/main/java/projet/ihm/controller/MainActivity.java
+++ app/src/main/java/projet/ihm/controller/MainActivity.java
@@ -23,11 +23,14 @@ import android.graphics.drawable.Drawable;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -58,6 +61,11 @@ public class MainActivity extends FragmentActivity implements IGPSActivity, OnMa
     private Location currentLocation;
     private GoogleMap mMap;
     private View fragmentInfoIncident;
+
+
+    // APPEL 18
+    private static final int MY_PERMISSION_REQUEST_CODE_CALL_PHONE = 555;
+    private static final String LOG_TAG = "AndroidExample";
 
 
     @Override
@@ -91,13 +99,9 @@ public class MainActivity extends FragmentActivity implements IGPSActivity, OnMa
             startActivity(intentSend);
         });
 
-        // bouton appeler
-        (findViewById(R.id.buttonCall)).setOnClickListener( click -> {
-            Intent intentSend = new Intent( getApplicationContext(), CallActivity.class);
-            startActivity(intentSend);
-        });
-
-
+        (findViewById(R.id.buttonCall)).setOnClickListener( click ->
+            askPermissionAndCall()
+        );
 
     }
 
@@ -271,6 +275,26 @@ public class MainActivity extends FragmentActivity implements IGPSActivity, OnMa
                     alertDialog.show();
                 }
             } break;
+
+            case MY_PERMISSION_REQUEST_CODE_CALL_PHONE: {
+
+                // Note: If request is cancelled, the result arrays are empty.
+                // Permissions granted (CALL_PHONE).
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    Log.i( LOG_TAG,"Permission granted!");
+                    Toast.makeText(this, "Permission granted!", Toast.LENGTH_LONG).show();
+
+                    this.callNow();
+                }
+                // Cancelled or denied.
+                else {
+                    Log.i( LOG_TAG,"Permission denied!");
+                    Toast.makeText(this, "Permission denied!", Toast.LENGTH_LONG).show();
+                }
+                break;
+            }
         }
     }
 
@@ -295,5 +319,58 @@ public class MainActivity extends FragmentActivity implements IGPSActivity, OnMa
 
         // after generating our bitmap we are returning our bitmap.
         return BitmapDescriptorFactory.fromBitmap(bitmap);
+    }
+
+
+    private void askPermissionAndCall() {
+
+        // With Android Level >= 23, you have to ask the user
+        // for permission to Call.
+        // 23
+
+        // Check if we have Call permission
+        int sendSmsPermisson = ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.CALL_PHONE);
+
+        if (sendSmsPermisson != PackageManager.PERMISSION_GRANTED) {
+            // If don't have permission so prompt the user.
+            this.requestPermissions(
+                    new String[]{Manifest.permission.CALL_PHONE},
+                    MY_PERMISSION_REQUEST_CODE_CALL_PHONE
+            );
+            return;
+        }
+        this.callNow();
+    }
+
+    private void callNow() {
+        String phoneNumber = "18";
+
+        Intent callIntent = new Intent(Intent.ACTION_CALL);
+        callIntent.setData(Uri.parse("tel:" + phoneNumber));
+        try {
+            this.startActivity(callIntent);
+        } catch (Exception ex) {
+            Toast.makeText(getApplicationContext(),"Your call failed... " + ex.getMessage(),
+                    Toast.LENGTH_LONG).show();
+            ex.printStackTrace();
+        }
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == MY_PERMISSION_REQUEST_CODE_CALL_PHONE) {
+            if (resultCode == RESULT_OK) {
+                // Do something with data (Result returned).
+                Toast.makeText(this, "Action OK", Toast.LENGTH_LONG).show();
+            } else if (resultCode == RESULT_CANCELED) {
+                Toast.makeText(this, "Action Cancelled", Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(this, "Action Failed", Toast.LENGTH_LONG).show();
+            }
+        }
     }
 }
